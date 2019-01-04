b0d5e21614f84e48d54572e88134db519daef9ba
p01arst0rm <polar@ever3st.com>
2019-01-04 17:06:36 +0000

    added new filters to interactive mode

--- Twelete/main.py
+++ Twelete/main.py
@@ -14,10 +14,12 @@ class twelete:
         #----------------------------------------------------------------------------
         api_key_set = False
         access_token_set = False
+
         
         filter_date = False
         filter_activity = False
         filter_media = False
+
         
         filter_phrase_black = False
         filter_phrase_white = False
@@ -30,34 +32,41 @@ class twelete:
                 self.api_key=api_key
                 self.api_key_secret=api_key_secret
 
+
         def set_access_tokens(self, access_token, access_token_secret):
                 twelete.access_token_set = True
                 self.access_token=access_token
                 self.access_token_secret=access_token_secret
                 
+
         def set_date_filter(self, x, y):
                 twelete.filter_date = True
                 self.date_min=dateutil.parser.parse(str(x + " 00:00:00 +0000"))
                 self.date_max=dateutil.parser.parse(str(y + " 00:00:00 +0000"))
+
 
         def set_activity_filter(self, x, y):
                 twelete.filter_activity = True
                 self.rt_min=x
                 self.like_min=y
                 
+
         def set_media_filter(self, x):
                 twelete.filter_media = True
                 self.media_filter = x
+
 
         def set_phrase_blacklist_filter(self, x):
                 twelete.filter_phrase_black = True
                 for a in x:
                         self.phrase_list_black.append(a)
 
+
         def set_phrase_whitelist_filter(self, x):
                 twelete.filter_phrase_white = True
                 for a in x:
                         self.phrase_list_white.append(a)
+
                 
         def set_keybase_twitter(self,x):
                 a = "Verifying myself: I am " + x + " on http://Keybase.io ."
@@ -74,15 +83,19 @@ class twelete:
                 with open(log_file, "a") as x:
                     x.write(log + "\n")
            
+
         def log_notify(self, notif):
                 self.log_handle(str("[INFO]: "+notif),self.notify_log_file)
+
 
         def log_warn(self, warn):
                 self.log_handle(str("[WARNING]: "+warn),self.warn_log_file)
 
+
         def log_err(self, err):
                 self.log_handle(str("[ERROR]: "+err),self.err_log_file)
                 sys.exit()
+
 
         def err_tweepy(self, err):
                 # format dodgy returns from tweepy api
@@ -101,6 +114,8 @@ class twelete:
                                 self.log_warn(str("{} ({})").format(x['message'], x['code']))
 
 
+
+
         # runtime requirement fetching
         # ---------------------------------------------------------------------------
         def get_query(self, request):
@@ -117,6 +132,7 @@ class twelete:
                         except ValueError:
                                 self.log_warn("Please enter \"Y\" or \"N\"")
 
+
         def get_num(self, request):
                 self.log_notify(request)
                 while True:
@@ -126,6 +142,7 @@ class twelete:
                                 return response
                         except ValueError:
                                 self.log_warn("Please enter a base 10 number.")  
+
 
         def get_date(self, request):
                 self.log_notify(request)
@@ -138,17 +155,21 @@ class twelete:
                         except ValueError:
                                 self.log_warn("Please enter a valid yyyy-mm-dd date.")
 
+
         def get_string(self, request, not_empty):
                 self.log_notify(request)
                 while True:
                         try:
                                 response = input("> ")
-                                if not_empty:
-                                        if response in ("", " "):
+                                if response in ("", " "):       
+                                        if not_empty:
                                                 raise ValueError()
+                                        else:
+                                                return False
                                 return response
                         except ValueError:
                                 self.log_warn("Please enter a value")
+
 
         def get_archive_dir(self):
                 self.log_notify("Please enter the path to your twitter .zip archive")
@@ -167,6 +188,7 @@ class twelete:
                         except PermissionError:
                                 self.log_warn(str("cannot open \"{}\" (invalid permsisions).").format(response))
 
+
         def get_api_tokens(self):
                 self.set_api_key(
                         self.get_string("Please enter your API Key", True),
@@ -175,6 +197,7 @@ class twelete:
                 self.set_access_tokens(
                         self.get_string("Please enter your Access Token", False),
                         self.get_string("Please enter your Access Token Secret", False))
+
 
         def get_filters(self):
                 if self.get_query("Filter based on date? (Y/N):"):
@@ -186,17 +209,7 @@ class twelete:
                        self.set_activity_filter(
                                 self.get_num("Please enter the minimum retweet requirement"),
                                 self.get_num("Please enter the minimum like requirement"))
-
-                if self.get_query("Filter based on included text (Y/N):"):
-                        filter_list = []
-                        while True:
-                                response = self.get_string("enter string to filter by; Press enter to finish entering.", False)
-                                if response in ("", " "):
-                                        break
-                                else:
-                                        filter_list.append(response)
-                        self.set_phrase_blacklist_filter(filter_list)
-                        
+                
                 if self.get_query("Filter based on media contents (Y/N):"):
                         while True:
                                 response = self.get_string("delete tweets WITH media or delete tweets WITHOUT media? (WITH/WITHOUT)\n", True)
@@ -206,6 +219,31 @@ class twelete:
                                 elif response.lower() in ("without"):
                                         self.set_media_filter(False)
                                         break
+
+                if self.get_query("blacklist phrases in text? (Y/N):"):
+                        filter_list = []
+                        while True:
+                                response = self.get_string("enter string to filter by; Press enter to finish entering.", False)
+                                if response == False:
+                                        break
+                                else:
+                                        filter_list.append(response)
+                        self.set_phrase_blacklist_filter(filter_list)
+
+                if self.get_query("whitelist phrases in text? (Y/N):"):
+                        filter_list = []
+                        while True:
+                                response = self.get_string("enter string to filter by; Press enter to finish entering.", False)
+                                if response == False:
+                                        break
+                                else:
+                                        filter_list.append(response)
+                        self.set_phrase_whitelist_filter(filter_list)
+                        
+                if self.get_query("preserve keybase proof? (Y/N):"):
+                        response = self.get_string("please enter your keybase username.", True)
+                        self.set_keybase_twitter(response)
+
                                 
         def get_confirm(self):
                 print("-----------------------------------")
@@ -225,6 +263,8 @@ class twelete:
                 return response
 
 
+
+
         # archive management
         # ---------------------------------------------------------------------------
         def read_file(self, file_name):
@@ -236,6 +276,7 @@ class twelete:
                 data.sort(key=lambda x: x['created_at'])
                 return data
 
+
         def load_js(self):
                 self.log_notify("fetching tweets from excracted archive..")
                 self.file_list = glob.glob(str(self.extract_dir) + "/data/js/tweets/*.js")
@@ -244,6 +285,7 @@ class twelete:
                 else:
                         self.file_list.sort()
                         self.log_notify("..Done.")
+
 
         def unzip_archive(self):
                 self.log_notify("fetching tweet archive from \"{}\"..".format(self.archive_dir))
@@ -260,12 +302,15 @@ class twelete:
                         self.log_err("cannot locate twitter archive.")
 
 
+
+
         # prerequisites
         # ---------------------------------------------------------------------------
         def fetch_archive(self):
                 self.extract_dir = os.path.splitext(self.archive_dir)[0]
                 self.unzip_archive()
                 self.load_js()
+
 
         def fetch_api(self):
                 self.log_notify("Loading api..")
@@ -283,6 +328,8 @@ class twelete:
                 self.log_notify("..api loaded.")
         
 
+
+
         # filter checks
         # ---------------------------------------------------------------------------
         def check_date(self, tweet):
@@ -294,6 +341,7 @@ class twelete:
                         if tweet_date > self.date_max:
                                 return True
                 return False
+
         
         def check_activity(self):
                 # check activity boundaries of status
@@ -304,6 +352,7 @@ class twelete:
                         if self.status.favorite_count >= self.like_min:
                                 return True
                 return False
+
         
         def check_media(self, tweet):
                 # check if status contains media
@@ -313,6 +362,7 @@ class twelete:
                         else:
                                 return not self.media_filter
                 return False
+
 
         def check_phrase_black(self, tweet):
                 # check if status contains blacklisted phrase
@@ -324,6 +374,7 @@ class twelete:
                                         return False
                         return True
                 return False
+
 
         def check_phrase_white(self, tweet):
                 # check if status contains whitelisted phrase
@@ -346,6 +397,7 @@ class twelete:
                 except UnicodeEncodeError:
                         log_warn("unprintable unicode in tweet")
 
+
         def fetch_status(self, tweet):
                 # try to fetch status from twitter
                 while True:
@@ -367,6 +419,7 @@ class twelete:
                         except tweepy.TweepError as e:
                                  if not self.err_tweepy(e):
                                         break
+
              
         def delete_tweets(self):
                 self.fetch_archive()
@@ -374,7 +427,8 @@ class twelete:
                 for file_name in self.file_list:
                         data = self.read_file(file_name)
                         for tweet in data:
-                                print(self.check_media(tweet))
+                                print(self.phrase_list_black)
+                                print(self.phrase_list_white)
                                 if self.check_date(tweet):
                                         continue
                                 if self.fetch_status(tweet):
@@ -409,6 +463,7 @@ class twelete:
                 else:
                         self.log_notify("No tweets deleted. Exiting...")
                 sys.exit()
+
                 
         def __init__(self):
                 # date filters
